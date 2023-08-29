2030bae1debcea301c8e8028c8478f5e7fb6729a
user <user@LAPTOP-2DK3J1EI>
2023-08-28 22:06:58 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Ingrese 3 numeros enteros diferentes:");
+int n1 = int.Parse(Console.ReadLine());
+int n2 = int.Parse(Console.ReadLine());
+int n3 = int.Parse(Console.ReadLine());
+if (n1 > n2 && n1 > n3)
+{
+    Console.WriteLine("El mayor es: " + n1);
+}
+else if (n2 > n1 && n2 > n3)
+{
+    Console.WriteLine("El mayor es: " + n2);
+}
+else if (n3 > n1 && n3 > n2)
+{
+    Console.WriteLine("El mayor es: " + n3);
+}
